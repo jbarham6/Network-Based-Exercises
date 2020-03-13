d3ed03d2bf890cb636693e2906b4fba8aa15f9bf
jbarham6 <jbarham6@uncc.edu>
2020-03-13 12:46:10 -0400

    Exercise 5 Complete

--- routes/courseDetails.js
+++ routes/courseDetails.js
@@ -2,14 +2,24 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var router = express.Router();
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var count = 0;
 
 
 router.get('/',function (req,res) {
-res.render('index');
+res.render('index', {data: count});
 });
+var myLogger = function (req, res, next) {
+    count++;
+    console.log(count);
+    next();
+};
+
+router.use(myLogger);
 
 router.post('/details',urlencodedParser, function(req, res) {
     var theCourse = require('./../models/Course');
+
+
     theCourse = {
         courseID: req.body.course,
         title: req.body.title,
@@ -18,9 +28,11 @@ router.post('/details',urlencodedParser, function(req, res) {
     };
     console.log(theCourse);
     if(theCourse.courseID == '' || theCourse.title == '' || theCourse.term == '' || theCourse.instructor == ''){
-        res.render('index');
+        res.render('index', {data: count});
+
     }else{
         res.render('details', {data: theCourse});
+
     }
 
 });
